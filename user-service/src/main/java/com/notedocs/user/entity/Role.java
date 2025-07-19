baf95db689dd59452b46fd98690d58e4d1d2abe9
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-07-19 13:42:06 +0300

    establish the relationship between user and role and role with Authority

---
+++ user-service/src/main/java/com/notedocs/user/entity/Role.java
@@ -0,0 +1,35 @@
+package com.notedocs.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String position;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "roles_authorities",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id")
+    )
+    private Set<Authority> authorities;
+
+}
