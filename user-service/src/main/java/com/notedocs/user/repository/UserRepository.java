4b915ce0a51fee6e26061f6003e4edc5d8520a1d
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-07-20 17:24:33 +0300

    adding two extra methods to find by email and username

--- user-service/src/main/java/com/notedocs/user/repository/UserRepository.java
+++ user-service/src/main/java/com/notedocs/user/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.notedocs.user.repository;
 import com.notedocs.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
