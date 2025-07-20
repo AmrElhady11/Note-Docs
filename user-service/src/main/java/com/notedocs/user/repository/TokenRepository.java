6c6b308712786c214d3afff7a669e3a095ccc4bd
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-07-20 17:20:20 +0300

    creating findAllToken method and its query

--- user-service/src/main/java/com/notedocs/user/repository/TokenRepository.java
+++ user-service/src/main/java/com/notedocs/user/repository/TokenRepository.java
@@ -2,6 +2,16 @@ package com.notedocs.user.repository;
 
 import com.notedocs.user.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
+    @Query("""
+SELECT t FROM Token t inner join User u on t.user.id =u.id
+where u.id=:userId and (t.expired=false or t.revoked =false )
+
+""")
+    List<Token> findAllValidTokensByUserId(int userId);
 }
