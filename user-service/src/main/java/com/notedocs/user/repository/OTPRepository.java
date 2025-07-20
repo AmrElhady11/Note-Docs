e797289bd8245d119d593416c4cf1643f5d2fd67
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-07-20 17:22:26 +0300

    creating deletebyuserid method and its query

--- user-service/src/main/java/com/notedocs/user/repository/OTPRepository.java
+++ user-service/src/main/java/com/notedocs/user/repository/OTPRepository.java
@@ -2,6 +2,12 @@ package com.notedocs.user.repository;
 
 import com.notedocs.user.entity.OTP;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface OTPRepository extends JpaRepository<OTP, Integer> {
+    Optional<OTP> findByOtp(String otp);
+    @Query("delete from OTP where OTP.user.id= :userID")
+    void deleteByUserId(int userID);
 }
