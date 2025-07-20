b55e76d89c9ec595a6a2f4f283d5788630de181e
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-07-20 17:17:03 +0300

    deleting unwanted imported libraries

--- user-service/src/main/java/com/notedocs/user/service/JWTService.java
+++ user-service/src/main/java/com/notedocs/user/service/JWTService.java
@@ -1,19 +1,8 @@
 package com.notedocs.user.service;
 
-import com.notedocs.user.entity.User;
-import com.notedocs.user.repository.TokenRepository;
-import com.notedocs.user.repository.UserRepository;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import lombok.AllArgsConstructor;
+
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-import java.security.Key;
 import java.util.*;
-import java.util.function.Function;
 
 public interface JWTService {
     public String generateToken(Map<String , Object>extraClaims, UserDetails user);
